6a744034a7889cddda64f1009befe3005c2fb691
Deelfc <84929683+Deelfc@users.noreply.github.com>
2024-06-03 01:19:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const byteSize = (str) => {
-  // write your code here
-};
+function byteSize(str) {
+    // Create a Blob object with the given string
+    const blob = new Blob([str]);
+    // Get the size of the Blob object in bytes
+    const sizeInBytes = blob.size;
+    return sizeInBytes;
+}
 
-// Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+// Example usage:
+console.log(byteSize('hello world')); // Should print 11
+console.log(byteSize('안녕하세요')); // Should print 15
+console.log(byteSize('')); // Should print 0
+
